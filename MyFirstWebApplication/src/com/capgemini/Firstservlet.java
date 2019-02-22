b49a2a8cba007c8b0a79d68d9b6669abcd3165d3
nam2515 <46920027+nam2515@users.noreply.github.com>
2019-02-22 14:34:25 +0530

    Add files via upload

---
+++ MyFirstWebApplication/src/com/capgemini/Firstservlet.java
@@ -0,0 +1,56 @@
+package com.capgemini;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet(name = "Firstservlet", urlPatterns = { "/Firstservlet" })
+public class Firstservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Firstservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pr=response.getWriter();
+		String choice=request.getParameter("Operation");
+		int x=Integer.parseInt(request.getParameter("value1"));
+		int y=Integer.parseInt(request.getParameter("value2"));
+		String sum,sub,mul;
+		switch(choice)
+		{
+		case "Add": pr.println(x+y);
+				break;
+		case "Mul": pr.println(x*y);
+				break;
+		case "Sub": pr.println(x-y);
+				break;
+		}
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ MyServlet/src/com/capgemini/MyServlet.java
@@ -0,0 +1,57 @@
+package com.capgemini;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pr= response.getWriter();
+		String userName=request.getParameter("userName");
+		String password=request.getParameter("password");
+		if(userName.equals("Naman") && password.equals("password"))
+		{
+			request.setAttribute("name", userName);
+			RequestDispatcher rd =request.getRequestDispatcher("Success");
+			rd.forward(request, response);
+			
+		}
+		else
+		{
+			response.sendRedirect("Error.html");
+		}
+	}
+
+}
